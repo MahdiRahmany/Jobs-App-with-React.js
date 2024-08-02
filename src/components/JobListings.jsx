2be62a964d8912f19693112c1f2a4a4ba78a4668
Mahdi Rahmani <mahdirahmani.main@gmail.com>
2024-08-02 18:41:46 +0330

    proxying

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -10,8 +10,8 @@ const JobListings = ({ isHome = false }) => {
 
     const fetchJobs = async () => {
       const apiUrl = isHome
-      ? "http://localhost:8000/jobs?_limit=3"
-        : "http://localhost:8000/jobs";
+      ? "http:/api/jobs?_limit=3"
+        : "http:/api/jobs";
       try {
         const res = await fetch(apiUrl);
         const data = await res.json();
