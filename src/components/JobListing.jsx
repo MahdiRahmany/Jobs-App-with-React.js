31d472ca251358bfed9e47566c43a9ba151fd6dc
Mahdi Rahmani <mahdirahmani.main@gmail.com>
2024-08-02 21:06:12 +0330

    single page Output

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FaMapMarked } from "react-icons/fa";
+import { FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const JobListing = ({ job }) => {
@@ -34,7 +34,7 @@ const JobListing = ({ job }) => {
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
-            <FaMapMarked className='inline text-lg mb-1 mr-1' /> 
+            <FaMapMarker className='inline text-lg mb-1 mr-1' /> 
             {job.location}
           </div>
           <Link
