6ed8f69e148386a7a4ae93bfd98ffeddaecfe21b
Mahdi Rahmani <mahdirahmani.main@gmail.com>
2024-08-03 03:04:34 +0330

    React Toastify Package

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,10 +1,24 @@
-import { useParams, useLoaderData } from "react-router-dom";
+import { useParams, useLoaderData, useNavigate } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
-const JobPage = () => {
+const JobPage = ({ deleteJob }) => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const job = useLoaderData();
+
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm("Are you sure want to delete this listing?");
+
+    if (!confirm) return;
+
+    deleteJob(jobId);
+
+    toast.success('Job deleted successfully')
+
+    navigate("/jobs");
+  };
 
   return (
     <>
